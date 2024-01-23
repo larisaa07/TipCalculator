c96c5145bda6cb6d7d0ed87f4c5139d3bd3ae1d5
larisaa07 <larisa_aveti@yahoo.com>
2024-01-23 14:56:01 -0500

    js progress

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 //Function to calculate and display tip, Total bill, and amount per person
 function calculateAndDisplay(){
-    
+    //Get the values from the html form
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    let numOfPpl = parseInt(document.getElementById('numOfPeople').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
+
+    //Calculate the tip, total bill, and amt per person
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill(bill, tip)
+    let amtPerPers = calcAmtPerPers(totalBill, numOfPpl)
+
+    //Disply the results in the html
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText = 'Total Bill: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText = 'Amount Per Person: $' + amtPerPers.toFixed(2)
 }
