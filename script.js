9223c9e85d476d8042865efdc5b35fa5ee7cb618
larisaa07 <larisa_aveti@yahoo.com>
2024-01-25 13:34:40 -0500

    done

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function calculateAndDisplay(){
     document.getElementById('totalBillResult').innerText = 'Total Bill: $' + totalBill.toFixed(2)
     document.getElementById('amtPerPersonResult').innerText = 'Amount Per Person: $' + amtPerPers.toFixed(2)
 }
+
+//Helper funtion to calculate the tip based on bill amount ans service quality
+function calculateTip(bill, quality){
+    if (quality === 'Great'){
+        return bill * .2
+    } else if (qualify === 'Good'){
+        return bill * .15
+    } else {
+        return bill * .10
+    }
+}
+
+//helper function to calculate the total tip by adding the bill and tip
+function calculateTotalBill(bill, tip){
+    return bill + tip
+}
+
+//helper function to calculate the amount each person should pay by dividing the total bill by the number of people
+function calcAmtPerPers(totalBill, numOfPpl){
+    return totalBill/numOfPpl
+}
